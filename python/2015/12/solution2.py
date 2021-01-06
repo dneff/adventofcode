1e2a948a004fa69db5597d2c91d6c318a76bcbf6
Dan Neff <dan@neff.cc>
2021-01-06 05:17:02 -0800

    solves day 12. happy with elegance of this-- granted I don't do much json parsing in my day work so this may have been obvious to a non-n00b.

---
+++ python/2015/12/solution2.py
@@ -0,0 +1,36 @@
+import json
+
+def printSolution(x):
+    print(f"The solution is: {x}")
+
+
+def getValue(data):
+    result = 0
+    if isinstance(data, str) or isinstance(data, int):
+        try:
+            return int(data)
+        except ValueError:
+            pass
+    elif isinstance(data, list):
+        for d in data:
+            result += getValue(d)
+    elif isinstance(data, dict):
+        if 'red' not in data.values():
+            for d in data.values():
+                result += getValue(d)
+
+    return result
+
+
+def main():
+
+    file = open("input.txt", "r")
+
+    json_data = file.read()
+    data = json.loads(json_data)
+
+    printSolution(getValue(data))
+
+
+if __name__ == "__main__":
+    main()
