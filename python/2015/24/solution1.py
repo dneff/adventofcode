a1500a53983ede8ce13288a4f420c406e5d8c8e4
Dan Neff <dan@neff.cc>
2021-01-25 20:58:52 -0800

    solves day 24. a bit of cheating as the minimum QE didn't require additional validation.

---
+++ python/2015/24/solution1.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+from functools import reduce
+
+def printSolution(x):
+    print(f"The solution is: {x}")
+
+
+def main():
+
+    file = open("input.txt", "r")
+    numbers = [int(x) for x in file.readlines()]
+    target = sum(numbers) // 3
+
+    max_len = 1
+    while sum(numbers[:max_len]) <= target:
+        max_len += 1
+    min_len = 1
+    while sum(numbers[-min_len:]) <= target:
+        min_len += 1
+
+    buckets = set()
+    min_combo = len(numbers)
+    qe = {}
+
+    for l in range(min_len, max_len + 1):
+        combos = combinations(numbers, l)
+        for x in combos:
+            if sum(x) == target:
+                buckets.add(x)
+                min_combo = min(min_combo, len(x))
+                qe[reduce((lambda a, b: a*b), x)] = x
+
+    print(min(qe))
+
+
+if __name__ == "__main__":
+    main()
