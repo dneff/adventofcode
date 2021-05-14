e4a2b5591186e11faa7ca49f6757f118348abb24
Dan Neff <dan@neff.cc>
2021-05-13 19:33:35 -0700

    solution to day 9. took a bit to figure out the elegant recursion.

--- python/2016/09/solution1.py
+++ python/2016/09/solution1.py
@@ -3,31 +3,30 @@ def printSolution(x):
 
 
 def decompress(s):
-    result = ''
+    result = 0
     index = 0
 
     while index < len(s):
         if s[index] != '(':
-            result += s[index]
+            result += 1
         else:
             index += 1
             end_marker = index + s[index:].find(')')
             marker = s[index:end_marker]
             characters, repeat = [int(x) for x in marker.split('x')]
             index = end_marker + 1
-            result += s[index:index+characters] * repeat
+            substring = s[index:index+characters]
+            result += len(substring) * repeat
             index += characters - 1
         index += 1
 
     return result
 
-
 def main():
     file = open("input.txt", "r")
 
-    data = file.readlines()[0]
-
-    printSolution(len(decompress(data)))
+    for data in file.readlines():
+        printSolution(decompress(data.strip()))
 
 
 if __name__ == "__main__":
