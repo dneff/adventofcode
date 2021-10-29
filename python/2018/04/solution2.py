437845bb7d8c04f8a4e0423e96bbf9e477d80aa6
Dan Neff <dneff@adobe.com>
2021-10-29 08:39:25 -0700

    solves day 4

---
+++ python/2018/04/solution2.py
@@ -0,0 +1,51 @@
+
+from collections import defaultdict
+from datetime import datetime
+
+def printSolution(x):
+    print(f"The solution is {x}")
+
+def main():
+    log = []
+    file = open('input.txt', 'r')
+    for line in file:
+        log.append(line.strip())
+    log.sort()
+
+    date_format= '%Y-%m-%d %H:%M'
+
+    guards = defaultdict(lambda: defaultdict(int))
+    current_guard = ''
+
+    idx = 0
+    while idx < len(log):
+        entry = log[idx].split()
+        if entry[2] == 'Guard':
+            current_guard = int(entry[3][1:])
+        elif entry[2] == 'falls':
+            start = log[idx].split(']')[0][1:]
+            start = datetime.strptime(start, date_format)
+            idx += 1
+            end = log[idx].split(']')[0][1:]
+            end = datetime.strptime(end, date_format)
+            for m in range(start.minute, end.minute):
+                guards[current_guard][m] += 1
+
+        idx += 1
+
+    guard_slept = [(x,sum(y.values())) for x,y in guards.items()]
+    guard_slept.sort(key = lambda x: x[1], reverse=True)
+
+    guard_top_minute = []
+    for guard in guards.keys():
+        sleepiest_minute = max(guards[guard], key=guards[guard].get)
+        sleepiest_count = guards[guard][sleepiest_minute]
+        guard_top_minute.append((guard, sleepiest_minute, sleepiest_count))
+
+    guard_top_minute.sort(key = lambda x: x[-1], reverse=True)
+    printSolution(guard_top_minute[0][0] * guard_top_minute[0][1])
+    
+
+
+if __name__ == "__main__":
+    main()
