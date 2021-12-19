4037f5f134672a1ce867bf80ec0dc73817994a4e
Dan Neff <dan@neff.cc>
2021-12-19 14:43:15 -0800

    progess on 19

--- python/2021/19/solution1.py
+++ python/2021/19/solution1.py
@@ -4,17 +4,27 @@ import copy
 def printSolution(x):
     print(f"The solution is {x}")
 
+
 class Scanner:
     def __init__(self):
         self.id = 0
         self.beacons = []
-        self.orientation = (0,0,0)
+        self.location = (0,0,0)
+        self.rotation = 0
 
     def add(self, coordinates):
         self.beacons.append(coordinates)
 
     def rotate(self, rotations):
         pass
+
+
+class Beacon:
+    def __init__(self):
+        self.location = (0,0,0)
+        self.neighors = []
+        self.scanner_count = 0
+
 
 def main():
     test = 'test.txt'
@@ -23,19 +33,19 @@ def main():
     active = test
 
     scanners = []
-    current_scanner = []
+    current_scanner = Scanner()
 
     file = open(active, 'r')
     for line in file.readlines():
         if len(line.strip()) == 0:
-            scanners.append(copy.copy(current_scanner))
-            current_scanner.clear()
+            scanners.append(current_scanner)
+            current_scanner = Scanner()
             continue
         elif line[0] == '-':
             continue
         else:
             x, y, z = [int(x) for x in line.strip().split(',')]
-            current_scanner.append([x, y, z])
+            current_scanner.add((x, y, z))
 
     scanners.append(copy.copy(current_scanner))
     current_scanner.clear()
