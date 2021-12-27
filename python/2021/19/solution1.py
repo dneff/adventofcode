4e90e3e2f1f0d2c8c45b7d2639e9c35d18b6b8c4
Dan Neff <dan@neff.cc>
2021-12-27 08:04:22 -0800

    progress on 19

--- python/2021/19/solution1.py
+++ python/2021/19/solution1.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import copy
+import math
 
 
 def printSolution(x):
@@ -9,7 +11,8 @@ class Scanner:
     def __init__(self):
         self.id = 0
         self.beacons = []
-        self.location = (0,0,0)
+        self.vectors = {}
+        self.location = (0, 0, 0)
         self.rotation = 0
 
     def add(self, coordinates):
@@ -18,10 +21,18 @@ class Scanner:
     def rotate(self, rotations):
         pass
 
+    def calculateVectors(self):
+        for s_idx in range(len(self.beacons)):
+            for d_idx in range(s_idx + 1, len(self.beacons)):
+                distance = (d - s for s, d in zip(self.beacons[s_idx], self.beacons[d_idx]))
+                x, y, z = distance
+                vector = math.sqrt(x**2 + y**2 + z**2)
+                self.vectors[round(vector, 1)] = [s_idx, d_idx]
+
 
 class Beacon:
     def __init__(self):
-        self.location = (0,0,0)
+        self.location = (0, 0, 0)
         self.neighors = []
         self.scanner_count = 0
 
@@ -38,20 +49,28 @@ def main():
     file = open(active, 'r')
     for line in file.readlines():
         if len(line.strip()) == 0:
+            current_scanner.calculateVectors()
             scanners.append(current_scanner)
             current_scanner = Scanner()
             continue
-        elif line[0] == '-':
+        elif line[:3] == '---':
             continue
         else:
             x, y, z = [int(x) for x in line.strip().split(',')]
             current_scanner.add((x, y, z))
 
+    current_scanner.calculateVectors()
     scanners.append(copy.copy(current_scanner))
-    current_scanner.clear()
+    current_scanner = Scanner()
 
-    for s in scanners:
-        print(s)
+    shared = set(scanners[0].vectors.keys()).intersection(set(scanners[4].vectors.keys()))
+    shared_b = set()
+    for v in shared:
+        for b in scanners[0].vectors[v]:
+            shared_b.add(scanners[0].beacons[b])
+    for b in shared_b:
+        print(b)
+    print(len(shared_b))
 
 if __name__ == "__main__":
     main()
