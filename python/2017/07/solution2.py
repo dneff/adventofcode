5dc57e466837533aef7cf73e81a7888b82d72f6e
Dan Neff <dan@neff.cc>
2022-09-03 15:15:05 -0700

    progress on 07

---
+++ python/2017/07/solution2.py
@@ -0,0 +1,84 @@
+from collections import defaultdict
+
+
+def print_solution(x):
+    """ prints solution """
+    print(f"The solution is: {x}")
+
+
+class Node():
+    def __init__(self):
+        self.name = None
+        self.parent = None
+        self.weight = 0
+        self.children = []
+        self.childsum = 0
+
+    def __getitem__(self, i):
+        return self.children[i]
+
+
+def correctBalance(node):
+    child_weights = []
+    if len(node.children) == 0:
+        return node.weight
+    for child in node.children:
+        child_weights.append(correctBalance(child))
+    if len(set(child_weights)) > 1:
+        print(f"IMBALANCE {node.name} {child_weights}")
+        imbalanced = None
+        balanced = None
+        imbalanced_node = None
+        for k, v in child_weights.items():
+            if len(v) > 1:
+                balanced = k
+            else:
+                imbalanced = k
+                imbalanced_node = v[0]
+        correction = imbalanced - balanced
+        imbalanced_node.weight += correction
+        print_solution(imbalanced_node.weight)
+    total_weight = sum(child_weights) + node.weight
+    return total_weight
+
+
+def main():
+    """ calculates solution """
+    file = open('input.txt', 'r', encoding='utf-8')
+
+    node_hash = {}
+
+    # creates nodes without relationships
+    for line in file.readlines():
+        node_name, node_weight = line.strip().split()[:2]
+        node_weight = int(node_weight.strip('()'))
+        node_hash[node_name] = Node()
+        node_hash[node_name].name = node_name
+        node_hash[node_name].weight = node_weight
+
+    # second pass for relationships
+    file.seek(0)
+
+    # create parent/child relationships
+    for line in file.readlines():
+        values = line.strip().split()
+        if len(values) > 3:
+            parent = values[0]
+            for child in values[3:]:
+                child = child.strip(',')
+                node_hash[parent].children.append(node_hash[child])
+                node_hash[child].parent = node_hash[parent]
+
+    # look for node.parent = None
+    root = None
+    for k,v in node_hash.items():
+        if v.parent == None:
+            root = v
+            break
+
+    # correct balance if necessary
+    correctBalance(root)
+
+
+if __name__ == "__main__":
+    main()
