7df38c9289402bf71744f9d4e4dbcf46314d7a1e
Dan Neff <dneff@adobe.com>
2022-12-20 14:13:04 -0800

    solves day 6

---
+++ python/2022/06/solution1.py
@@ -0,0 +1,25 @@
+
+def printSolution(x):
+    print(f"The solution is: {x}")
+
+def markerEnd(s, size=4):
+    marker = []
+    for idx, char in enumerate(s):
+        marker.append(char)
+        if len(marker) < size:
+            continue
+        if len(marker) > size:
+            marker = marker[-size:]
+        if len(set(marker)) == size:
+            return idx + 1
+    return ValueError("No index found")
+
+
+def main():
+    file = open('../input/06.txt', 'r', encoding='utf-8')
+    for line in file.readlines():
+        end = markerEnd(line.strip())
+        printSolution(end)
+
+if __name__ == "__main__":
+    main()
