e9455a3da8728fbf5196587adfc6135517f81de5
Dan Neff <dneff@adobe.com>
2022-12-21 20:22:12 -0800

    solves day 8

--- python/2022/08/solution2.py
+++ python/2022/08/solution2.py
@@ -1,10 +1,19 @@
 
+from math import prod
+
 def printSolution(x):
     print(f"The solution is: {x}")
 
+def scorePosition(height, trees):
+    distance = 0
+    for tree in trees:
+        distance += 1
+        if tree >= height:
+            break    
+    return max(distance, 1)
 
 def main():
-    file = open('../input/08-test.txt', 'r', encoding='utf-8')
+    file = open('../input/08.txt', 'r', encoding='utf-8')
     forest = []
     for line in file.readlines():
         trees = [int(x) for x in line.strip()]
@@ -13,24 +22,27 @@ def main():
     edge_min = 0
     edge_max = len(forest) - 1 
 
-    visible = 0
+    bestScore = 0
 
     for y, trees in enumerate(forest):
         for x, tree in enumerate(trees):
             if edge_min in [x,y] or edge_max in [x,y]:
-                visible += 1
+                continue
             else:
-                left_trees = trees[:x]
-                right_trees = trees[x+1:]
+                left_trees = [forest[y][z] for z in range(edge_min,x)]
+                right_trees = [forest[y][z] for z in range(x+1,edge_max+1)]
                 up_trees = [forest[z][x] for z in range(edge_min,y)]
                 down_trees = [forest[z][x] for z in range(y+1,edge_max+1)]
-                if all([x < tree for x in left_trees]) or \
-                    all([x < tree for x in right_trees]) or \
-                    all([x < tree for x in up_trees]) or \
-                    all([x < tree for x in down_trees]):
-                    visible += 1
-    # 1216 too low
-    printSolution(visible)
+                # orient so side closest to treehouse has idx 0
+                left_trees = left_trees[::-1]
+                up_trees = up_trees[::-1]
+
+                visibility = []
+                for trees in [left_trees, right_trees, up_trees, down_trees]:
+                    visibility.append(scorePosition(forest[y][x], trees))
+                bestScore = max(bestScore, prod(visibility))
+
+    printSolution(bestScore)
 
 if __name__ == "__main__":
     main()
