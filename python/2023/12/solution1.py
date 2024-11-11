bee0be5890ac2f377d4a5cbd70aeef0b3718cb2d
Dan Neff <dan@neff.cc>
2024-11-11 11:37:44 -0800

    rewrites and solves parts 1 and 2

--- python/2023/12/solution1.py
+++ python/2023/12/solution1.py
@@ -1,82 +1,70 @@
-"""solves 2023 day 12 part 1"""
+import functools
 
-def print_solution(x):
-    """prints solution"""
-    print(f"The solution is: {x}")
+@functools.cache
+def calc (record, groups):
 
-def validate_record(springs, counts):
-    """check if spring positions match counts"""
-    positions = [len(x) for x in springs.split('.') if len(x) > 0]
-    return positions == counts
+    if not groups:
+        if "#" not in record:
+            return 1
+        else:
+            return 0
 
-def next_hamming(binary):
-    """
-    return next binary number with same hamming weight
-    algo source: https://math.stackexchange.com/a/2255452
-    """
-    c = binary & -binary
-    r = binary + c
-    next_binary = (((r^binary) >> 2) // c) | r
-    return next_binary
+    if not record:
+        return 0
 
-def spring_combos(springs, record):
-    """
-    generator that returns all spring combinations
-    use a bitmask, find max, min value of bit mask
-    from min val, find next hamming weight number
-    do that up to max val
-    """
-    hamming = sum(record) - springs.count('#')
-    mask_size = springs.count('?')
-    max_binary = int('1' * hamming + '0' * (mask_size - hamming),2)
-    current = int('0' * (mask_size - hamming) + '1' * hamming,2)
-    format_cfg = '0' + str(mask_size) + 'b'
-    while current <= max_binary:
-        mask = format(current, format_cfg)
-        mask = mask.replace('1','#').replace('0','.')
-        mask = list(mask)
-        valid_combo = []
-        for s in springs[:]:
-            if s == '?':
-                valid_combo.append(mask.pop(0))
+    next_char = record[0]
+    next_group = groups[0]
+
+    def pound():
+        this_group = record[:next_group]
+        this_group = this_group.replace("?", "#")
+
+        if this_group != next_group * "#":
+            return 0
+
+        if len(record) == next_group:
+            if len(groups) == 1:
+                return 1
             else:
-                valid_combo.append(s)
-        yield ''.join(valid_combo)
-        if current == max_binary:
-            break
-        current = next_hamming(current)
+                return 0
+        if record[next_group] in "?.":
+            return calc(record[next_group+1:], groups[1:])
+        
+        return 0
 
+    
+    def dot():
+        return calc(record[1:], groups)
+    
+    if next_char == '#':
+        out = pound()
+    elif next_char == '.':
+        out = dot()
+    elif next_char == '?':
+        out = dot() + pound()
+    else:
+        raise RuntimeError
+    
+    #print(f"{record} {groups} -> {out}")
+    #print(10*"-")
+
+    return out
 
 
 def main():
-    """main"""
-    lines = []
     filename = "./python/2023/input/12.txt"
     with open(filename, "r", encoding="utf-8") as file:
         lines = [line.strip() for line in file.readlines()]
 
-    valid_combos = []
-    total_records = len(lines)
-    current_records = 0
-    for record in lines:
-        springs, counts = record.split()
-        counts = [int(x) for x in counts.split(',')]
-        #print(springs, counts)
-        possible = spring_combos(springs, counts)
-        valid_count = 0
-        for p in possible:
-            valid = validate_record(p,counts)
-            #print(p, valid)
-            if valid:
-                valid_count += 1
-        valid_combos.append(valid_count)
-        current_records += 1
-        print(f"{current_records}/{total_records} complete")
+    output = 0
 
+    for entry in lines:
+            record, group = entry.split()
+            groups = [int(i) for i in group.split(',')]
 
-    #print(valid_combos)
-    print_solution(sum(valid_combos))
+            output += calc(record, tuple(groups))
 
+    print(f"The result is: {output}")
 
 if __name__ == "__main__":
     main()
