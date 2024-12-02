3e7aec407d0978779570a04e5a83e8b1dbfa7645
Dan Neff <dneff@adobe.com>
2024-12-01 16:23:44 -0800

    renames directory, cleans up formatting

--- python/2024/01/solution1.py
+++ python/2024/01/solution1.py
@@ -2,10 +2,9 @@ def print_solution(x):
     print(f"The solution is: {x}")
 
 
-
 def main():
     """finds solution"""
-    left, right = [],[]
+    left, right = [], []
     filename = "./python/2024/input/01.txt"
     with open(filename, "r", encoding="utf-8") as f:
         for line in f.readlines():
@@ -15,8 +14,9 @@ def main():
     left.sort()
     right.sort()
 
-    differences = [abs(x - y) for x,y in zip(left, right)]
+    differences = [abs(x - y) for x, y in zip(left, right)]
     print_solution(sum(differences))
+
 
 if __name__ == "__main__":
     main()
--- python/2024/01/solution2.py
+++ python/2024/01/solution2.py
@@ -1,18 +1,18 @@
 import collections
 
+
 def print_solution(x):
     print(f"The solution is: {x}")
 
 
-
 def main():
     """
-    figure out exactly how often each number from the left list 
-    appears in the right list. Calculate a total similarity score 
-    by adding up each number in the left list after multiplying it 
+    figure out exactly how often each number from the left list
+    appears in the right list. Calculate a total similarity score
+    by adding up each number in the left list after multiplying it
     by the number of times that number appears in the right list.
     """
-    left, right = [],[]
+    left, right = [], []
     filename = "./python/2024/input/01.txt"
     with open(filename, "r", encoding="utf-8") as f:
         for line in f.readlines():
@@ -21,10 +21,8 @@ def main():
 
     freq = collections.Counter(right)
     score = [x * freq[x] for x in left]
-    
-    print_solution(sum(score))
-    
 
+    print_solution(sum(score))
 
 
 if __name__ == "__main__":
